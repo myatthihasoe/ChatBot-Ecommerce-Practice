d88fed26c6b8714c1a1459421072941a3d45de0c
myatthihasoe <myatthihasoe@abank.com.mm>
2026-02-05 15:58:22 +0630

    use beforeEach to remove duplicate

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeAll, beforeEach } from "vitest";
 import Product from "./Product";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
@@ -6,8 +6,10 @@ import axios from "axios";
 vi.mock("axios"); //fake version of axios
 
 describe("Product Component", () => {
-  it("displays product details correctly", () => {
-    const product = {
+  let product;
+  const loadCart = vi.fn(); //create fake function that doesn't do anything
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -18,7 +20,8 @@ describe("Product Component", () => {
       priceCents: 1090,
       keywords: ["socks", "sports", "apparel"],
     };
-    const loadCart = vi.fn(); //create fake function that doesn't do anything
+  });
+  it("displays product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />); //display component of on page
     expect(
       screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs"),
@@ -36,18 +39,6 @@ describe("Product Component", () => {
   });
 
   it("adds a product to the cart", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-    const loadCart = vi.fn(); //create fake function that doesn't do anything
     render(<Product product={product} loadCart={loadCart} />);
     const user = userEvent.setup();
     const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
